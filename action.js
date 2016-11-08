ff7c383a9c02e1144cff68c1b08650231c550791
lihaoyun6 <lihaoyun11@gmail.com>
2016-11-09 02:31:51 +0800

    Update action.js

--- action.js
+++ action.js
@@ -10,6 +10,8 @@ web['磁力百科'] ={url:"http://www.cilibaike.net/word/{key}.html",encode:"utf
 
 web['遨游搜'] ={url:"http://www.aoyoso.com/search/{key}_ctime_1.html",encode:"utf8"};  
 
-web['必应磁链'] ={url:"http://cn.bing.com/search?q={key}磁力链接",encode:"utf8"};  
+web['必应磁链'] ={url:"http://cn.bing.com/search?q={key}磁力链接",encode:"utf8"}; 
+                             
+web['百度磁链'] ={url:"https://www.baidu.com/s?word={key}磁力链接",encode:"utf8"};
 
 $(".submitbox .submit").click(function(){	var sweb=$(this).val();    var url=web[sweb]['url'];   var keyword=""; if(web[sweb]['encode']=='utf8'){keyword=encodeURI($("#s_text").val() );  }else if (web[sweb]['encode']=='gb2312' ) {	keyword= URLEncodeGB2312($("#s_text").val() ) ; } if(keyword==""){  if(window.confirm('关键词为空是否打开'+sweb+'首页')){ 	 window.open(url.match(/http[s]?:\/\/(.*?)([:\/]|$)/)[0] );} }else{ window.open(url.replace("{key}",keyword) ); }	});});
